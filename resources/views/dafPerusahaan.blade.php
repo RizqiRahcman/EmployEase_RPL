e5e974175b57805785ce3794515fe57c82c3cf73
NonimusAnimus <nonimusanimus@gmail.com>
2024-05-26 18:23:35 +0700

    update hal carikerja & daftar perusahaan

--- resources/views/dafPerusahaan.blade.php
+++ resources/views/dafPerusahaan.blade.php
@@ -12,8 +12,10 @@
     
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
-    rel="stylesheet"/>
+    <link 
+      href="https://fonts.googleapis.com/css2?family=Assistant:wght@200..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+      rel="stylesheet"
+    />
     
     <!-- CSS style  -->
     <link rel="stylesheet" href="css_daf/style_daf.css" />
@@ -37,10 +39,10 @@
           data-target="#navbarNav"
           aria-controls="navbarNav"
           aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+          aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
@@ -104,42 +106,56 @@
     </div>
     
     <!-- Kategori perusahaan -->
-    <div class="container">
-      <h1 style="color: #454545; font-weight: bold;  margin: 100px 0px 20px;">Kategori Perusahaan</h1>
+    <div class="container py-5">
+      {{-- <h1 style="color: #454545; font-weight: bold;  margin: 100px 0px 20px;">Kategori <span>Perusahaan</span></h1> --}}
+      <div class="section-tittle mt-3">
+        <h1>Katergori <span>Pekerjaan</span></h1>
+        <p class="text-right">
+          <a href="#">Lihat semua lowongan </a>
+        </p>
+      </div>
       <div class="row g-4 text-center" style="margin-top: 24px;">
         
         <!-- row 1 kategori -->
         <div class="col-lg-3 col-sm-6">
-          <div class="category theme-shadow p-lg-5 p-4">
-            <div class="iconbox">
-              <i data-feather="monitor"></i>
+          <a href="#">
+            <div class="category theme-shadow p-lg-5 p-4">
+              <div class="iconbox">
+                <i data-feather="monitor"></i>
+              </div>
+              <h5 class="mt-4 mb-3">Teknologi</h5>
             </div>
-            <h5 class="mt-4 mb-3">Teknologi</h5>
-          </div>
+          </a>
         </div>
         <div class="col-lg-3 col-sm-6">
-          <div class="category theme-shadow p-lg-5 p-4">
-            <div class="iconbox">
-              <i data-feather="pen-tool"></i>
+          <a href="#">
+            <div class="category theme-shadow p-lg-5 p-4">
+              <div class="iconbox">
+                <i data-feather="pen-tool"></i>
+              </div>
+              <h5 class="mt-4 mb-3">Desain</h5>
             </div>
-            <h5 class="mt-4 mb-3">Desain</h5>
-          </div>
+          </a>
         </div>
         <div class="col-lg-3 col-sm-6">
-          <div class="category theme-shadow p-lg-5 p-4">
-            <div class="iconbox">
-              <i data-feather="shopping-cart"></i>
+          <a href="#">
+            <div class="category theme-shadow p-lg-5 p-4">
+              <div class="iconbox">
+                <i data-feather="shopping-cart"></i>
+              </div>
+              <h5 class="mt-4 mb-3">Pemasaran</h5>
             </div>
-            <h5 class="mt-4 mb-3">Pemasaran</h5>
-          </div>
+          </a>
         </div>
         <div class="col-lg-3 col-sm-6">
-          <div class="category theme-shadow p-lg-5 p-4">
-            <div class="iconbox">
-              <i data-feather="dollar-sign"></i>
+          <a href="#">
+            <div class="category theme-shadow p-lg-5 p-4">
+              <div class="iconbox">
+                <i data-feather="dollar-sign"></i>
+              </div>
+              <h5 class="mt-4 mb-3">Keuangan</h5>
             </div>
-            <h5 class="mt-4 mb-3">Keuangan</h5>
-          </div>
+          </a>
         </div>
       </div> 
 
@@ -147,60 +163,288 @@
       <div class="row g-5 text-center" style="margin-top: 24px; margin-bottom: 24px;">
 
         <div class="col-lg-3 col-sm-6 ">
-          <div class="category theme-shadow p-lg-5 p-4">
-            <div class="iconbox">
-              <i data-feather="database"></i>
-            </div>
-            <h5 class="mt-4 mb-3">Bisnis</h5>
-          </div>
-        </div>
-        
-        <div class="col-lg-3 col-sm-6">
-          <div class="category theme-shadow p-lg-5 p-4">
-            <a href="#">
+          <a href="#">
+            <div class="category theme-shadow p-lg-5 p-4">
               <div class="iconbox">
-                <i data-feather="truck"></i>
+                <i data-feather="database"></i>
               </div>
-            </a>
-            <h5 class="mt-4 mb-3">Logistik</h5>
-          </div>
-        </div>
-        
-        <div class="col-lg-3 col-sm-6">
-          <div class="category theme-shadow p-lg-5 p-4">
-            <div class="iconbox">
-              <i data-feather="activity"></i>
+              <h5 class="mt-4 mb-3">Bisnis</h5>
             </div>
-            <h5 class="mt-4 mb-3">Kesehatan</h5>
-          </div>
+          </a>
         </div>
-
         <div class="col-lg-3 col-sm-6">
-          <div class="category theme-shadow p-lg-5 p-4">
-            <div class="iconbox">
-              <i data-feather="shuffle"></i>
+          <a href="#">
+            <div class="category theme-shadow p-lg-5 p-4">
+                <div class="iconbox">
+                  <i data-feather="truck"></i>
+                </div>
+              <h5 class="mt-4 mb-3">Logistik</h5>
             </div>
-            <h5 class="mt-4 mb-3">Lain-lain</h5>
-          </div>
+          </a>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <a href="#">
+            <div class="category theme-shadow p-lg-5 p-4">
+              <div class="iconbox">
+                <i data-feather="activity"></i>
+              </div>
+              <h5 class="mt-4 mb-3">Kesehatan</h5>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <a href="#">
+            <div class="category theme-shadow p-lg-5 p-4">
+              <div class="iconbox">
+                <i data-feather="shuffle"></i>
+              </div>
+              <h5 class="mt-4 mb-3">Lain-lain</h5>
+            </div>
+          </a>
         </div>
       </div>
     </div>
+  </a>
 
     <!-- perusahaan rekomendasi -->
-    <div class="container">
-      <h1 style="color: #454545; font-weight: bold; margin-top: 100px;">Perusahaan Rekomendasi</h1>
-      <h6>Paling banyak dilamar</h6>
-
+    <div class="container py-5">
+      <section class="section-padding mt-5">
+      <h1 style="color: #454545; font-weight: bold;  margin: 100px 0px 20px;">Perusahaan Rekomendasi</h1>
       <div class="row g-4 text-left" style="margin-top: 24px;">
-        <div class="col-lg-4 col-sm-6">
-          <div class="category theme-shadow p-lg-5 p-4">
-            <div class="iconbox">
-              <i data-feather="monitor"></i>
+        <div class="cardjob col-md-6 col-lg-4">
+          <div class="card job-card">
+            <div class="card-body">
+              <div class="d-flex align-items-center mb-3 me-3">
+                <div class="company-logo ">
+                  <img
+                    src="image/logo pertamina.png"
+                    alt="logo pt"
+                  />
+                </div>
+                <h5 class="card-title mb-0">Admin</h5>
+              </div>
+              <h6 class="card-subtitle mb-2 text-muted">
+                Pertamina - Semarang, Jawa Tengah
+              </h6>
+              <p class="card-text">
+                Pertamina mencari marketer planner yang dapat meny...
+              </p>
+              <a href="#" class="job-category me-2">Marketing</a>
+              <a href="#" class="job-category">Design</a>
             </div>
-            <h5 class="mt-4 mb-3">Teknologi</h5>
+          </div>
+        </div> 
+        <div class="col-md-6 col-lg-4">
+          <div class="card job-card">
+            <div class="card-body">
+              <div class="d-flex align-items-center mb-3">
+                <div class="company-logo me-3">
+                  <img
+                    src="image/logo pertamina.png"
+                    alt="logo pt"
+                  />
+                </div>
+                <h5 class="card-title mb-0">Human Capital</h5>
+              </div>
+              <h6 class="card-subtitle mb-2 text-muted">
+                Pertamina - Semarang, Jawa Tengah
+              </h6>
+              <p class="card-text">
+                Pertamina mencari marketer planner yang dapat meny...
+              </p>
+              <a href="#" class="job-category me-2">Marketing</a>
+              <a href="#" class="job-category">Design</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4">
+          <div class="card job-card">
+            <div class="card-body">
+              <div class="d-flex align-items-center mb-3">
+                <div class="company-logo me-3">
+                  <img
+                    src="image/logo pertamina.png"
+                    alt="logo pt"
+                  />
+                </div>
+                <h5 class="card-title mb-0">Marketing Planner</h5>
+              </div>
+              <h6 class="card-subtitle mb-2 text-muted">
+                Pertamina - Semarang, Jawa Tengah
+              </h6>
+              <p class="card-text">
+                Pertamina mencari marketer planner yang dapat meny...
+              </p>
+              <a href="#" class="job-category me-2">Marketing</a>
+              <a href="#" class="job-category">Design</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4">
+          <div class="card job-card">
+            <div class="card-body">
+              <div class="d-flex align-items-center mb-3">
+                <div class="company-logo me-3">
+                  <img
+                    src="image/logo pertamina.png"
+                    alt="logo pt"
+                  />
+                </div>
+                <h5 class="card-title mb-0">IT Support</h5>
+              </div>
+              <h6 class="card-subtitle mb-2 text-muted">
+                Pertamina - Semarang, Jawa Tengah
+              </h6>
+              <p class="card-text">
+                Pertamina mencari marketer planner yang dapat meny...
+              </p>
+              <a href="#" class="job-category me-2">Marketing</a>
+              <a href="#" class="job-category">Design</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4">
+          <div class="card job-card">
+            <div class="card-body">
+              <div class="d-flex align-items-center mb-3">
+                <div class="company-logo me-3">
+                  <img
+                    src="image/logo pertamina.png"
+                    alt="logo pt"
+                  />
+                </div>
+                <h5 class="card-title mb-0">Frontend Developer</h5>
+              </div>
+              <h6 class="card-subtitle mb-2 text-muted">
+                Pertamina - Semarang, Jawa Tengah
+              </h6>
+              <p class="card-text">
+                Pertamina mencari marketer planner yang dapat meny...
+              </p>
+              <a href="#" class="job-category me-2">Marketing</a>
+              <a href="#" class="job-category">Design</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4">
+          <div class="card job-card">
+            <div class="card-body">
+              <div class="d-flex align-items-center mb-3">
+                <div class="company-logo me-3">
+                  <img
+                    src="image/logo pertamina.png"
+                    alt="logo pt"
+                  />
+                </div>
+                <h5 class="card-title mb-0">Ui Designer</h5>
+              </div>
+              <h6 class="card-subtitle mb-2 text-muted">
+                Pertamina - Semarang, Jawa Tengah
+              </h6>
+              <p class="card-text">
+                Pertamina mencari marketer planner yang dapat meny...
+              </p>
+              <a href="#" class="job-category me-2">Marketing</a>
+              <a href="#" class="job-category">Design</a>
+            </div>
           </div>
         </div>
       </div>
+      </section>
+    </div>
+      <!-- Footer -->
+      <footer class="footer-dark">
+        <div class="container mt-3">
+          <div class="row">
+            <div class="col-sm-6 col-md-4 footer-column">
+              <img src="image/logo_footer.png" alt="logo" />
+              <p>
+                Platform yang sangat ideal bagi mereka yang ingin menemukan
+                pekerjaan impian dan tertarik dengan karier menjanjikan.
+              </p>
+            </div>
+            <div class="col-sm-6 col-md-4 footer-column text-center">
+              <h4>About</h4>
+              <ul class="list-unstyled">
+                <li><a href="#">Companies</a></li>
+                <li><a href="#">Terms</a></li>
+                <li><a href="#">Advice</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+              </ul>
+            </div>
+            <div class="col-sm-6 col-md-3 footer-column">
+              <h4>Resources</h4>
+              <ul class="list-unstyled">
+                <li><a href="#">Help Docs</a></li>
+                <li><a href="#">Guides</a></li>
+                <li><a href="#">Updates</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="row border-top">
+            <div class="col-6 text-end mt-3">
+              <p>2024 @ EmployEase. All rights reserved</p>
+            </div>
+            <div class="col-6 footer-column social-icons mt-3">
+              <a href="#"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  class="bi bi-instagram"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"
+                  /></svg
+              ></a>
+              <a href="#"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  class="bi bi-twitter-x"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"
+                  /></svg
+              ></a>
+              <a href="#"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  class="bi bi-linkedin"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"
+                  /></svg
+              ></a>
+              <a href="#"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  class="bi bi-facebook"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"
+                  /></svg
+              ></a>
+            </div>
+          </div>
+        </div>
+      </footer>
+      
+
     </div>
 
 
@@ -208,8 +452,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/f2c387131d.js" crossorigin="anonymous"></script>
     <script>
-      src="script.js"
       feather.replace();
     </script>
+    <script src="jquery/jquery-3.7.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
   </body>
 </html>
