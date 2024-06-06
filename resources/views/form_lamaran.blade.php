9ecac66aa18da946b29520b270bd792b537aa436
alifaaziz <alifabdulaziz24@gmail.com>
2024-06-06 17:51:48 +0700

    merge

--- resources/views/form_lamaran.blade.php
+++ resources/views/form_lamaran.blade.php
@@ -8,7 +8,7 @@
     <title>Form Lamaran | Employease</title>
 
     {{-- bs --}}
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 
     {{-- css --}}
     <link rel="stylesheet" href="css_carikerja/form.css">
@@ -20,7 +20,109 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <style>
+        :root {
+            --c-dark: #212529;
+            --c-brand: #ff6000;
+            --c-brand-light: #ff914f;
+            --c-brand-rgb: 255, 96, 0;
+            --body: #727272;
+            --c-box-shadow: 0px 15px 25px rgba(0, 0, 0, 0.08);
+            --transition: all 0.5s ease;
+            }
+            body {
+            background: var(--c-brand);
+            background: linear-gradient(to right, var(--c-brand), var(--c-brand-light));
+            }
 
+            /* Head */
+
+            .head {
+            padding: 20px 0px;
+            }
+            .col-3{
+            width: 120px;
+            height: 120px;
+            }
+            .col-7{
+            padding-left: 0;
+            }
+            .judul_pekerjaan{
+            margin-bottom: 0px;
+            }
+            .detail_pekerjaan{
+            margin: 8px 0px 8px 0px;
+            }
+            /* 
+            .judul_pekerjaan{
+            margin-left: 12px;
+            width: 420px;
+            } */
+
+            .h6 {
+            color: #727272;
+            justify-items: left;
+            width: 200px;
+            font-weight: lighter;
+            }
+            .img-thumbnail {
+            width: 120px; /* Lebar container gambar */
+            height: 120px; /* Tinggi container gambar */
+            /* background-image: url('path/to/image.jpg'); URL gambar yang akan ditampilkan */
+            /* background-size: cover; Menyesuaikan gambar agar sesuai dengan ukuran container */
+            background-position: left; /* Posisi gambar di tengah container */
+            background-repeat: no-repeat; /* Tidak mengulang gambar */
+            }
+
+            .fas{
+            padding-right: 8px;
+            color: var(--c-brand);
+            }
+
+            /* deskripsi */
+            .keterangan{
+            padding: 0px 0px 20px 0px;
+            }
+            .isi_ket{
+            text-align: justify;
+            padding: 0px 12px;
+            }
+
+            /* ///////////// */
+
+            .btn-lg{
+            background-color: var(--c-brand);
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            border-radius: 4px;
+            }
+
+            .btn:hover{
+            background-color: var(--c-dark);
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            border-radius: 4px;
+            }
+
+            .d-grid .disabled{
+            background-color: var(--c-brand);
+            color: white;
+            }
+    </style>
 </head>
 <body>
     <!-- Bootstrap 5 Contact Form Snippet -->
@@ -52,8 +154,8 @@
                         </div>
         
                         <div class="konten_form">
-                            <form method="POST" action="{{ route('lamaran.store') }}" id="contactForm">
-                                {{ csrf_field() }}
+                            <form id="contactForm" data-sb-form-api-token="API_TOKEN" method="POST" action="{{ route('lamaran.store') }}" id="contactForm">
+                                @csrf
                                 <h3>Lamar pekerjaan</h3>
 
                                 <!-- Hidden pekerjaan_id Input -->
@@ -61,26 +163,34 @@
 
                                 <!-- Name -->
                                 <div class="form-floating mb-3">
-                                    <label for="nama">Nama lengkap Anda</label>
-                                    <input class="form-control" id="nama" name="nama" type="text" value="{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}" readonly />
+                                    <div class="col">
+                                        <label for="nama" class="mb-2">Nama lengkap Anda</label>
+                                        <input class="form-control" id="nama" name="nama" type="text" value="{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}" readonly />
+                                    </div>
                                 </div>
 
                                 <!-- Email -->
                                 <div class="form-floating mb-3">
-                                    <label for="emailAddress">Email Address</label>
+                                    <div class="col">
+                                    <label for="emailAddress" class="mb-2">Email Address</label>
                                     <input class="form-control" id="emailAddress" name="email" type="email" value="{{ Auth::user()->email }}" readonly />
+                                    </div>
                                 </div>
 
                                 <!-- Phone Number Input -->
                                 <div class="form-floating mb-3">
-                                    <label for="phoneNumber">Phone Number</label>
-                                    <input class="form-control" id="phoneNumber" name="phone" type="tel" placeholder="081234567890" required />
+                                    <div class="col" class="mb-2">
+                                        <label for="phoneNumber">Phone Number</label>
+                                        <input class="form-control" id="phoneNumber" name="phone" type="tel" placeholder="Nomor telp anda" required />
+                                    </div>
                                 </div>
 
                                 <!-- Message Input -->
                                 <div class="form-floating mb-3">
+                                    <div class="col" class="mb-2">
                                     <label for="message">Surat lamaran</label>
                                     <textarea class="form-control" id="message" name="message" placeholder="Tuliskan surat lamaran anda" style="height: 10rem;" required></textarea>
+                                    </div>
                                 </div>
 
                                 <!-- Submit button -->
