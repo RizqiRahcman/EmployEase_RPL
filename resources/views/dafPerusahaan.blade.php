afe7b903350df415fda41b7b703f47777f6a4686
unknown <mikalintang@gmail.com>
2024-06-10 15:04:15 +0700

    update loren ipsum daftar perusahaan

--- resources/views/dafPerusahaan.blade.php
+++ resources/views/dafPerusahaan.blade.php
@@ -114,8 +114,8 @@
     <div class="head">
       <div class="bg-dark"></div>
       <figure class="text-center">
-        <h1 style="color: #FFE6C7; font-weight: bold;">A well-known quote, contained in a blockquote element.</h1>
-        <p style="color: #FFE6C7;">Unlock Your Next Career Opportunity Today!</p>
+        <h1 style="color: #FFE6C7; font-weight: bold;">uild your dream career by finding the perfect company.</h1>
+        <p style="color: #FFE6C7;">Discover Your Dream Job Here!</p>
       </figure>
       <form class="d-flex">
         <div class="input-group" style="margin-right: 4px;">
